1fef01981216afcbe366e531f72eceac246b954b
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-10-05 23:54:05 -0400

    Create a placeholder main file to run the interpreter

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,20 @@
 module Main (main) where
 
+import Language.Lambda.Targets.Interpreter (InterConfig (..), runInterpreter)
+import qualified System.Environment as SE
+import System.IO
+
+conf :: InterConfig
+conf =
+    InterConfig
+        { prefix = "λ "
+        }
+
 main :: IO ()
-main = pure ()
+main = do
+    hSetBuffering stdout NoBuffering
+
+    args <- SE.getArgs
+    case args of
+        ["-i", filename] -> runInterpreter conf filename
+        _ -> putStrLn "currently is garbage, only does '-i filename'"
